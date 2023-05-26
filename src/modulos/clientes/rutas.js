6baf04c0a5a6a6b39c6a7524d8f6b565e74e1817
kevin120196 <kevintalaverac120196@gmail.com>
2023-05-26 17:45:47 -0600

    metodo actualizar y agregar

--- src/modulos/clientes/rutas.js
+++ src/modulos/clientes/rutas.js
@@ -12,31 +12,46 @@ const controlador = require('./controlador')
 router.get('/', todos);
 router.get('/:id', uno);
 router.put('/', eliminar);
+router.post('/', agregar)
 
-async function todos (req,res) {
+async function todos (req,res,next) {
     try {
         const items = await controlador.todos();
         respuesta.success(req,res,items,200);
     } catch (error) {
-        respuesta.error(req,res,err,500);
+      next(error);
     }
 };
 
-async function uno (req,res) {
+async function uno (req,res,next) {
     try {
         const items = await controlador.show(req.params.id);
         respuesta.success(req,res,items,200);       
     } catch (error) {
-        respuesta.error(req,res,error,500);
+        next(error);
     }
 };
 
-async function eliminar (req,res) {
+async function eliminar (req,res,next) {
     try {
         const items = await controlador.eliminar(req.body);
         respuesta.success(req,res,'item eliminado satisfactoriamente',200);       
     } catch (error) {
-        respuesta.error(req,res,error,500);
+        next(error);
+    }
+};
+
+async function agregar (req,res,next) {
+    try {
+        const items = await controlador.agregar(req.body);
+        if (req.body.id == 0)  {
+            mensaje = "item guardado con exito";
+        }else{
+            mensaje = "item actualizado con exito";
+        }
+        respuesta.success(req,res,mensaje,201);       
+    } catch (error) {
+        next(error);
     }
 };
 
